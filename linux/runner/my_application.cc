9a704bcd7dea3b61c8aa9a586fb7b379ecf7d3d4
Austin Graham <austingraham731@gmail.com>
2025-03-27 23:54:27 -0700

    Create pipeline and empty project (#1)
    * add github actions
    
    * fix header bar on Linux
    
    * re-scaffold test
    
    * add pull request template
    
    * update action branch name
    
    * add codecov to actions
    
    * fix test and macos build command

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -14,6 +14,8 @@ struct _MyApplication {
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
+const gchar* APP_TITLE = "DDA Desktop";
+
 // Implements GApplication::activate.
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
@@ -40,11 +42,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "dda_desktop");
+    gtk_header_bar_set_title(header_bar, APP_TITLE);
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "dda_desktop");
+    gtk_window_set_title(window, APP_TITLE);
   }
 
   gtk_window_set_default_size(window, 1280, 720);
